c1ebcf12a927f6de20dc99252b9f07e64b2dced4
Jason Shaw <yourhoward@gmail.com>
2020-10-21 23:53:33 -0700

    removed relay implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useSetRecoilState } from 'recoil'
+
 import Home from './pages/Home'
 import { AllQuestions } from './store/atoms'
 // eslint-disable-next-line
 import { mockQuestionArray } from './utils/mockData'
 
 type Props = {
-  fetchCards: () => Promise<FirebaseDocArray>
+    fetchCards: () => Promise<FirebaseDocArray>
 }
 
 // eslint-disable-next-line react/prop-types
 const App: React.FC<Props> = ({fetchCards}) => {
     const setAllQuestions = useSetRecoilState(AllQuestions)
 
-    React.useEffect(() => {
-        fetchCards()
-            .then(cards => {
-                setAllQuestions(cards)
-            })
-    // setAllQuestions(mockQuestionArray)
-    }, [fetchCards, setAllQuestions])
+    useEffect(() => {
+        fetchCards().then(cards => setAllQuestions(cards))
+        // setAllQuestions(mockQuestionArray)
+    },[fetchCards, setAllQuestions])
 
     return (
         <Home />
